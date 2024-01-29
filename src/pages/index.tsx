294a0044fc33fa349afd5c2e1e96ac4923cbe821
Angelika Szczerbowska <andzia913@gmail.com>
2024-01-29 14:21:53 +0100

    deleted navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 import "../styles/main.scss";
 import Header from "../components/Header";
 import Contact from "../components/Contact";
@@ -11,7 +10,6 @@ import About from "../components/About";
 const IndexPage: React.FC = () => {
   return (
     <main>
-      <NavBar />
       <Header />
       <Technologies />
       <ProjectsContainer />
