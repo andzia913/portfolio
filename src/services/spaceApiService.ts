4de4c97c8c667c6eb3e18f7f0a93d1eb6fffa704
Angelika Szczerbowska <andzia913@gmail.com>
2024-02-08 08:44:40 +0100

    ApiService refactor from class to function

--- src/services/spaceApiService.ts
+++ src/services/spaceApiService.ts
@@ -1,27 +1,25 @@
 import axios from "axios";
 
-class ApiService {
-  static async fetchData(countOfRandomImages?: number, date?: string) {
-    const apiKey = process.env.GATSBY_NASA_API_KEY;
-    let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+const ApiService = async (countOfRandomImages?: number, date?: string) => {
+  const apiKey = process.env.GATSBY_NASA_API_KEY;
+  let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
-    if (countOfRandomImages) {
-      url += `&count=${countOfRandomImages}`;
-    }
-
-    if (date) {
-      url += `&date=${date}`;
-    }
-
-    try {
-      const response = await axios.get(url);
-      console.log("Response:", response.data);
-      return response.data;
-    } catch (error) {
-      console.log("Error:", error);
-      return null;
-    }
+  if (countOfRandomImages) {
+    url += `&count=${countOfRandomImages}`;
   }
-}
+
+  if (date) {
+    url += `&date=${date}`;
+  }
+
+  try {
+    const response = await axios.get(url);
+    console.log("Response:", response.data);
+    return response.data;
+  } catch (error) {
+    console.log("Error:", error);
+    return null;
+  }
+};
 
 export default ApiService;
